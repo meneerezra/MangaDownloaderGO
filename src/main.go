1574235bc744090ab3d65d8dd54e8b09a10926d6
EzraSchutte <ezraschutte227@gmail.com>
2025-02-28 11:14:20 +0100

    Feat: Loop through array to print all details about every manga in mangalist

--- src/main.go
+++ src/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"mangaDownloaderGO/fetcher"
+	"mangaDownloaderGO/storage"
+	"os"
 )
 
 func main() {
-	fetcher.FetchManga("Eromanga-sensei")
+	fetcher.FetchManga(os.Args[1])
+	for _, manga := range storage.GetMangaList() {
+		fmt.Println("ID: " + manga.ID)
+		fmt.Println("Title: " + manga.MangaTitle)
+		fmt.Printf("Chapters: %v\n", manga.ChapterCount)
+		fmt.Println("")
+	}
 }
+
