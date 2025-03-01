312ec1c0f9ed452a4c0a751289826c750e78d040
EzraSchutte <ezraschutte227@gmail.com>
2025-03-01 19:15:48 +0100

    Minor: Moved mangadex structs to obj/mangadex

--- src/fetcher/mangadex.go
+++ src/fetcher/mangadex.go
@@ -13,43 +13,6 @@ import (
 
 var MangaDexUrl string
 
-type MangaDexMangaResponse struct {
-	Data []MangaDexMangaDataItem `json:"data"`
-	_    struct{}                `json:"-"`
-}
-
-type MangaDexMangaDataItem struct {
-	ID         string          `json:"id"`
-	Attributes MangaAttributes `json:"attributes"`
-}
-
-type MangaAttributes struct {
-	Title map[string]string `json:"title"`
-}
-
-type MangaDexChapterResponse struct {
-	Data []MangaDexChapterDataItem `json:"data"`
-	_    struct{}                  `json:"-"`
-}
-
-type MangaDexChapterDataItem struct {
-	ID            string                 `json:"id"`
-	Attributes    ChapterAttributes      `json:"attributes"`
-	Relationships []ChapterRelationShips `json:"relationships"`
-}
-
-type ChapterAttributes struct {
-	Title    string `json:"title"`
-	Volume   string `json:"volume"`
-	Chapter  string `json:"chapter"`
-	Language string `json:"translatedLanguage"`
-}
-
-type ChapterRelationShips struct {
-	ID   string `json:"id"`
-	Type string `json:"type"`
-}
-
 func SetURL(url string) {
 	MangaDexUrl = url
 }
@@ -92,7 +55,7 @@ func FetchMangas(mangaTitle string) ([]obj.Manga, error) {
 	}
 
 	var fetchedMangas []obj.Manga
-	var mangadexResponse MangaDexMangaResponse
+	var mangadexResponse obj.MangaDexMangaResponse
 	if err := json.Unmarshal(body, &mangadexResponse); err != nil {
 		return nil, err
 	}
@@ -148,7 +111,7 @@ func GetChaptersFromManga(manga obj.Manga, params url.Values) ([]obj.Chapter, er
 		panic(err)
 	}
 
-	var mangadexResponse MangaDexChapterResponse
+	var mangadexResponse obj.MangaDexChapterResponse
 
 	if err := json.Unmarshal(body, &mangadexResponse); err != nil {
 		return nil, err
---
+++ src/obj/mangadex.go
@@ -0,0 +1,38 @@
+package obj
+
+type MangaDexMangaResponse struct {
+	Data []MangaDexMangaDataItem `json:"data"`
+	_    struct{}                `json:"-"`
+}
+
+type MangaDexMangaDataItem struct {
+	ID         string          `json:"id"`
+	Attributes MangaAttributes `json:"attributes"`
+}
+
+type MangaAttributes struct {
+	Title map[string]string `json:"title"`
+}
+
+type MangaDexChapterResponse struct {
+	Data []MangaDexChapterDataItem `json:"data"`
+	_    struct{}                  `json:"-"`
+}
+
+type MangaDexChapterDataItem struct {
+	ID            string                 `json:"id"`
+	Attributes    ChapterAttributes      `json:"attributes"`
+	Relationships []ChapterRelationShips `json:"relationships"`
+}
+
+type ChapterAttributes struct {
+	Title    string `json:"title"`
+	Volume   string `json:"volume"`
+	Chapter  string `json:"chapter"`
+	Language string `json:"translatedLanguage"`
+}
+
+type ChapterRelationShips struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
