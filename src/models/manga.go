4d7af62e9d45e7e210ec0053b4918ae48aab6231
EzraSchutte <ezraschutte227@gmail.com>
2025-03-01 11:57:30 +0100

    Feat: Added a []Chapter in manga struct and a Manga type in Chapter struct

--- src/models/manga.go
+++ src/models/manga.go
@@ -4,6 +4,7 @@ type Manga struct {
 	ID           string
 	MangaTitle   string
 	ChapterCount int
+	Chapters []Chapter
 }
 
 type Cover struct {
@@ -12,6 +13,7 @@ type Cover struct {
 
 type Chapter struct {
 	ID string
+	Manga Manga
 	Name string
 	ChapterNumber int
 	Volume int
